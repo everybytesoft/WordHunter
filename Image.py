637cad4983f6084782ee68e2228dcd5c4ddffb11
KharazyanLevon <166710371+KharazyanLevon@users.noreply.github.com>
2024-05-19 11:48:13 +0300

    Update Image.py

--- Image.py
+++ Image.py
@@ -13,7 +13,7 @@ def svg_grid(data):
     str = ''
     for i in range(6):
         for j in range(5):
-            str+=svg_square(data[i][j][1], data[i][j][0], j, i)
+            str+=svg_square(int(data[i][j][1]), data[i][j][0], j, i)
             
     svg = f'<svg xmlns="http://www.w3.org/2000/svg" width="162" height="194"><style>text{{font:24px monospace}}</style><defs><linearGradient id="0" gradientTransform="rotate(90)"><stop stop-color="#4f4f4f" offset="0"/><stop stop-color="#212121" offset="1"/></linearGradient><linearGradient id="1" gradientTransform="rotate(90)"><stop stop-color="#ffec8e" offset="0"/><stop stop-color="#d1ab00" offset="1"/></linearGradient><linearGradient id="2" gradientTransform="rotate(90)"><stop stop-color="#8eff97" offset="0"/><stop stop-color="#00d10e" offset="1"/></linearGradient></defs><rect fill="#000" width="162" height="194"/>{str}</svg>'
 
